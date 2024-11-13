10ca2e0939f3a67c1e98ec9176bcc8ec87b35500
Andhika Rangga <115131226+Andhikardg@users.noreply.github.com>
2024-11-13 22:41:42 +0700

    Update GSPDSHBRD.py

--- GSPDSHBRD.py
+++ GSPDSHBRD.py
@@ -29,7 +29,7 @@ credentials = {
   "project_id": st.secrets.project_id,
   "private_key_id": st.secrets.private_key_id,
   "private_key": st.secrets.private_key,
-  "client_id": st.secrets.project_id.client_id,
+  "client_id": st.secrets.client_id,
   "auth_uri": st.secrets.auth_uri,
   "token_uri": st.secrets.token_uri,
   "auth_provider_x509_cert_url": st.secrets.auth_provider_x509_cert_url,
